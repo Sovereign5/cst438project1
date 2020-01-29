b12cccbf5e3886a74522f8fe2a9bee2736f6139d
Chris <chrisgari@hotmail.com>
2020-01-28 17:23:00 -0800

    Initial commit

---
+++ app/src/main/java/com/example/cst438project1/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.cst438project1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Button Declarations
+    private Button mViewCourses;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Interface Definitions
+        mViewCourses = (Button) findViewById(R.id.courseButton);
+
+        mViewCourses.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchViewCourses();
+            }
+        });
+    }
+
+    private void launchViewCourses()
+    {
+        Intent intent = new Intent(this, ViewCourse.class);
+        startActivity(intent);
+    }
+}
