deed5b2050a19ea0742fc9ef4cbb5a65efdc2820
Quentin-Allen Velayo Fernandez <qfernandez@csumb.edu>
2020-02-11 17:24:49 -0800

    working on account page

---
+++ app/src/main/java/com/example/cst438project1/CreateAccount.java
@@ -0,0 +1,15 @@
+package com.example.cst438project1;
+
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateAccount extends AppCompatActivity implements OnClickListener {
+    private EditText firstnameText;
+    private EditText lastnameText;
+    private EditText usernameText;
+    private EditText passwordText;
+    private Button continueButton;
+}
